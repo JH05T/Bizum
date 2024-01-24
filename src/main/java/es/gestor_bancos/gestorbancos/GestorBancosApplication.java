b48b2c3b26625b2256690a7e063312547e5f0445
JH05T <gvjoseantonio215@gmail.com>
2024-01-24 10:34:13 +0100

    0

---
+++ src/main/java/es/gestor_bancos/gestorbancos/GestorBancosApplication.java
@@ -0,0 +1,13 @@
+package es.gestor_bancos.gestorbancos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorBancosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorBancosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/gestor_bancos/gestorbancos/GestorBancosApplicationTests.java
@@ -0,0 +1,13 @@
+package es.gestor_bancos.gestorbancos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorBancosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
